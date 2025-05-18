46ee7de06bc3e7bddd621811a0035c64f74df4aa
Gabriel de Oliveira <gabriel-ao@hotmail.com>
2025-05-17 22:58:57 -0300

    test: ensure BcryptAdapter throws if bcrypt throws

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -30,6 +30,17 @@ describe('Bcrypt Adapter', () => {
 
     expect(hash).toBe('hash');
   });
+
+  test('should throw if bcrypt.hash throws', async () => {
+    const sut = makeSut();
+    jest
+      .spyOn(bcrypt, 'hash')
+      .mockImplementationOnce(() => Promise.reject(new Error()));
+
+    const promise = sut.encrypt('any_value');
+
+    await expect(promise).rejects.toThrow();
+  });
 });
 
 // TODO - PARADO NO MINUTO 12:11 AULA 15
