8cfff276890e7189dd530b44fd46b67716ee1194
Gabriel de Oliveira <gabriel-ao@hotmail.com>
2025-09-20 02:19:41 -0300

    refactor: move mongodb methods to a helper

---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,19 @@
+import { MongoClient } from 'mongodb';
+
+export const MongoHelper = {
+  client: null as unknown as MongoClient,
+
+  async connect(uri: string): Promise<void> {
+    if (!process.env.MONGO_URL) {
+      throw new Error('MONGO_URL environment variable is not defined');
+    }
+    this.client = await MongoClient.connect(process.env.MONGO_URL);
+  },
+
+  async disconnect(): Promise<void> {
+    if (this.client) {
+      await this.client.close();
+      this.client = null as unknown as MongoClient;
+    }
+  },
+};
