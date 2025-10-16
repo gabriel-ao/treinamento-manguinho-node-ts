ce82f1a12289baf44c64882a7801d3bf0c9112e3
Gabriel de Oliveira <gabriel-ao@hotmail.com>
2025-10-16 00:45:02 -0300

    test: ensure LogControllerDecorator calls LogErrorRepository if controller returns a server error

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -4,11 +4,7 @@ import {
   HttpResponse,
 } from '../../presentation/protocols';
 import { LogControllerDecorator } from './log';
-
-interface SutTypes {
-  sut: LogControllerDecorator;
-  controllerStub: Controller;
-}
+import { LogErrorRepository } from '../../data/protocols/log-error-repository';
 
 const makeController = (): Controller => {
   class ControllerStub implements Controller {
@@ -26,11 +22,29 @@ const makeController = (): Controller => {
   return new ControllerStub();
 };
 
+const makeLogErrorRepository = (): LogErrorRepository => {
+  class LogErrorRepositoryStub implements LogErrorRepository {
+    async log(stack: string): Promise<void> {
+      return new Promise((resolve) => resolve());
+    }
+  }
+  return new LogErrorRepositoryStub();
+};
+
+interface SutTypes {
+  sut: LogControllerDecorator;
+  controllerStub: Controller;
+  LogErrorRepositoryStub: LogErrorRepository;
+}
 const makeSut = (): SutTypes => {
   const controllerStub = makeController();
+  const LogErrorRepositoryStub = makeLogErrorRepository();
   const handleSpy = jest.spyOn(controllerStub, 'handle');
-  const sut = new LogControllerDecorator(controllerStub);
-  return { sut, controllerStub };
+  const sut = new LogControllerDecorator(
+    controllerStub,
+    LogErrorRepositoryStub
+  );
+  return { sut, controllerStub, LogErrorRepositoryStub };
 };
 
 describe('LogControllerDecorator', () => {
@@ -71,5 +85,27 @@ describe('LogControllerDecorator', () => {
         email: 'any_email@mail.com',
       },
     });
+  });
+
+  test('Should call LogErrorRepository with correct error if controller returns a server error', async () => {
+    const { sut, controllerStub, LogErrorRepositoryStub } = makeSut();
+    const fakeError = new Error('stack trace');
+    fakeError.stack = 'any_stack';
+
+    const logSpy = jest.spyOn(LogErrorRepositoryStub, 'log');
+
+    jest.spyOn(controllerStub, 'handle').mockRejectedValueOnce(fakeError);
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    };
+
+    await sut.handle(httpRequest);
+    expect(logSpy).toHaveBeenCalledWith('any_stack');
   });
 });
