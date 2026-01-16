46898aac144c0e4a149a7f64d0010b6cb264682d
Gabriel de Oliveira <gabriel-ao@hotmail.com>
2026-01-15 21:38:35 -0300

    refactor: movee sut creation to a factory method

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -2,28 +2,37 @@ import { LoginController } from './login';
 import { badRequest } from '../../helpers/http-helper';
 import { MissingParamError } from '../../errors';
 
+interface SutTypes {
+  sut: LoginController;
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new LoginController();
+  return { sut };
+};
+
 describe('LoginController', () => {
   test('should return 400 if no email is provided', async () => {
-    const loginController = new LoginController();
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         password: 'any_password',
       },
     };
-    const httpResponse = await loginController.handle(httpRequest);
+    const httpResponse = await sut.handle(httpRequest);
     expect(httpResponse.statusCode).toEqual(
       badRequest(new MissingParamError('email')).statusCode
     );
   });
 
   test('should return 400 if no password is provided', async () => {
-    const loginController = new LoginController();
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         email: 'any_email@mail.com',
       },
     };
-    const httpResponse = await loginController.handle(httpRequest);
+    const httpResponse = await sut.handle(httpRequest);
     expect(httpResponse.statusCode).toEqual(
       badRequest(new MissingParamError('password')).statusCode
     );
